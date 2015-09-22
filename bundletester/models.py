cfc9d5408c2fd08084d14ed221c9b51024c386dd
Tim Van Steenburgh <tvansteenburgh@gmail.com>
2015-09-22 15:19:16 -0400

    Strip revision num off charm dir name

--- bundletester/models.py
+++ bundletester/models.py
@@ -6,6 +6,14 @@ import tempfile
 from bundletester import utils
 
 
+def is_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
 class FSEntity(dict):
     pass
 
@@ -13,11 +21,20 @@ class FSEntity(dict):
 class Charm(FSEntity):
     @classmethod
     def from_deployer_charm(cls, dcharm):
-        # Copy charm source from the deployer cache to a new temp location
-        # so we can change the charm dir name to match the charm name
-        # (some tests rely on these matching).
+        """Copy charm source from the deployer cache to a new temp location
+        so we can change the charm dir name to match the charm name
+        (some tests rely on these matching).
+
+        """
         tmp_dir = tempfile.mkdtemp()
-        charm_dir = os.path.join(tmp_dir, dcharm.name.split('/')[-1])
+        charm_name = dcharm.name.split('/')[-1]
+
+        # Strip off revision if there is one
+        name_parts = charm_name.split('-')
+        if is_int(name_parts[-1]):
+            charm_name = '-'.join(name_parts[:-1])
+
+        charm_dir = os.path.join(tmp_dir, charm_name)
         shutil.copytree(dcharm.path, charm_dir, symlinks=True)
         atexit.register(shutil.rmtree, tmp_dir)
 
